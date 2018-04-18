5987c458f4a2c2330824717d9438178c67201502
Jovialyy <zzychina1988@gmail.com>
2018-04-19 01:25:37 +0200

    test
    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+        File file = new File("e:\\read.csv");
+        FileReader fReader = new FileReader(file);
+        CSVReader csvReader = new CSVReader(fReader, ',');
+        String[] strs = csvReader.readNext();
+        if(strs != null && strs.length > 0){
+            for(String str : strs)
+                if(null != str && !str.equals(""))
+                    System.out.print(str + " , ");
+            System.out.println("\n---------------");
+        }
+        List<String[]> list = csvReader.readAll();
+        for(String[] ss : list){
+            for(String s : ss)
+                if(null != s && !s.equals(""))
+                    System.out.print(s + " , ");
+            System.out.println();
+        }
+        csvReader.close();
     }
 }
